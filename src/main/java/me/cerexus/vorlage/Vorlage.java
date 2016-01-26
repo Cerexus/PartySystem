a7a2727e07447c05c14d5f0d55d4134e79a4b727
Max Klug <maxklug1999@gmail.com>
2016-01-26 23:57:54 +0100

    Initial Commit.

---
+++ src/main/java/me/cerexus/vorlage/Vorlage.java
@@ -0,0 +1,14 @@
+package me.cerexus.vorlage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Vorlage extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
